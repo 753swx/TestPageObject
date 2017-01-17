4c7a8770c29468adb0bb24276b09e078abafc990
Artur Kananchuk <753swx@gmail.com>
2017-01-17 17:33:45 +0300

    PageFactory1.4

--- src/main/java/PreviousVersion/Tests.java
+++ src/main/java/PreviousVersion/Tests.java
@@ -1,6 +1,8 @@
 package PreviousVersion;
 
 import Patterns.Decorator.WebDriverDecorator;
+import PreviousVersion.AuthorizedPage;
+import PreviousVersion.LoginPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
